f64c49ec3cbffab50eeb0ed7426954aae96cfa2e
arkady <larytet@yahoo.com>
2018-05-06 18:25:17 +0300

    Add service

--- service/service.go
+++ service/service.go
@@ -7,7 +7,51 @@
 // all possible combinations of the collected port knocks and ports the service 
 // failed to bind to the server
  
- package main
+package main
 
+import (
+	"net"
+	"fmt"
+	"flag"
+//	"os"
+//	"port-knocking-ipc/utils"
+)
+
+func getPortsToBind() []int{
+	portsBase := *flag.Int("port_base", 21380, "Base port number")
+	portsRangeSize := *flag.Int("port_range", 10, "Size of the ports range")
+	ports := []int{}
+	for port := portsBase;port < (portsBase+portsRangeSize); port += 1 {
+		ports = append(ports, port)
+	}  	
+	return ports
+}
+
+func bindPorts(ports []int) []net.Listener{
+	listeners := []net.Listener{}
+	failedToBind := []int{}
+	for port := range ports {
+		name := fmt.Sprintf("localhost:%d", port)
+		listener, err := net.Listen("tcp", name)
+		if err == nil {
+			listeners = append(listeners, listener)
+		} else {
+			failedToBind = append(failedToBind, port)
+		}
+	}
+	if len(failedToBind) != 0 {
+		fmt.Printf("Failed to bind ports %v\n", failedToBind)		
+	}
+	return listeners	
+}
+
+func closeListeners(listeners []net.Listener) {
+	for _, listener := range listeners {
+		listener.Close()
+	}
+}
 func main() {
+	ports := getPortsToBind()
+	listeners := bindPorts(ports)
+	defer closeListeners(listeners)	
 }
